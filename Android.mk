f0e3646ea00417536de7b42d9197a020e624c15f
Wiktor Strzębała <wiktorek140@tlen.pl>
2020-03-10 09:09:50 +0100

    montana: distinct 4.9 build from normal one
    Signed-off-by: Wiktor Strzębała <wiktorek140@tlen.pl>

--- Android.mk
+++ Android.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2017-2019 The LineageOS Project
+# Copyright (C) 2018-2020 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,6 +16,6 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE),montana)
+ifeq ($(TARGET_DEVICE),montana_49)
 
 endif
